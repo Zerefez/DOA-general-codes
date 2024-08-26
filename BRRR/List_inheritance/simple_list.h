703c755933c1b4d78fb01e1a2514143416fe0479
Zerefez <johnguyen29801@gmail.com>
2024-08-26 16:48:09 +0200

    week 2 example codes DONE!

---
+++ BRRR/List_inheritance/simple_list.h
@@ -0,0 +1,17 @@
+#pragma once
+#pragma once
+
+template <typename Object>
+class List {
+
+public:
+	virtual int size() = 0;
+	virtual bool empty() = 0;
+	virtual void clear() = 0;
+	virtual void push_front(const Object x) = 0;
+	virtual void push_back(const Object x) = 0;
+	virtual Object pop_front() = 0;
+	virtual Object pop_back() = 0;
+	virtual Object find_kth(int pos) = 0;
+};
+
