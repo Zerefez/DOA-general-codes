78d992866870c49cfcbe982626c1b0e42c53260d
Zerefez <johnguyen29801@gmail.com>
2024-08-27 21:17:59 +0200

    adjusted X1

--- BRRR/X1/Vector.cpp
+++ BRRR/X1/Vector.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <vector>
-#include <algorithm> 
+#include <algorithm>
+
 
 using namespace std;
 
 int main() {
     // Initialize random seed
-    srand(time(0));
+    srand((0));
 
     int M, N;
     cout << "Enter the number of elements in the first vector (M): ";
@@ -14,34 +15,50 @@ int main() {
     cout << "Enter the number of random integers to check (N): ";
     cin >> N;
 
+    cout << endl;
+
     // Generate M random integers in the range [0, 99] and store them in a vector
     vector<int> vecM;
-    
+
     cout << "Vector M: ";
     for (int i = 0; i < M; ++i) {
         int randomNum = rand() % 100;
         vecM.push_back(randomNum);
         cout << randomNum << " ";
     }
-    cout << endl;
+    cout << endl<<endl;
 
-    // Generate N random integers in the same range and count how many are in vecM
-    int count = 0;
+    // Generate N random integers in the same range and store them in a vector
+    vector<int> vecN;
+    vector<int> matchedNumbers;
     cout << "Vector N: ";
     for (int i = 0; i < N; ++i) {
         int randomNum = rand() % 100;
+        vecN.push_back(randomNum);
         cout << randomNum << " ";
 
-        // Use an iterator to check if the random number is in vecM
+        // Check if the random number is in vecM and store matches
         vector<int>::iterator it = find(vecM.begin(), vecM.end(), randomNum);
-          if(it != vecM.end()) {
-            ++count;
-          }
+         if(it != vecM.end()) {
+            matchedNumbers.push_back(randomNum);
+        }
     }
-    cout << endl;
+    cout << endl << endl;
 
     // Output the result
-    cout << "Number of random integers found in the vector: " << count << endl;
+    cout << "Number of random integers found in the vector: " << matchedNumbers.size() << endl << endl;
+
+    // Print the matched numbers
+    if (!matchedNumbers.empty()) {
+        cout << "Matched numbers: ";
+        for (int num : matchedNumbers) {
+            cout << num << " ";
+        }
+        cout << endl << endl;
+    }
+    else {
+        cout << "No numbers from Vector N were found in Vector M." << endl;
+    }
 
     return 0;
 }
